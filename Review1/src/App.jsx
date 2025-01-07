0227e3df472c7b205c5a62b011d0d05ac6013bfa
sara <saradernouni@gmail.com>
2025-01-07 14:39:13 -0500

    6

--- Review1/src/App.jsx
+++ Review1/src/App.jsx
@@ -1,27 +1,15 @@
 import React from 'react'
-import List from './List'
-
-
-
-const fruits = [{id: 1, name:"apple", Calories:95}, 
-                {id: 2, name:"orange", Calories:45}, 
-                {id: 3, name:"banana", Calories:105}, 
-                {id: 4, name:"coconut", Calories:159}, 
-                {id: 5, name:"pineapple", Calories:37}];
-
-const vegetables = [{id: 6, name:"potatoes", Calories:110}, 
-                    {id: 7, name:"celery", Calories:15}, 
-                    {id: 8, name:"Carrots", Calories:25}, 
-                    {id: 9, name:"corn", Calories:63}, 
-                    {id: 10, name:"broccoli", Calories:50}];
-
+import Button from './Button'
+import ProfilePicture from './ProfilePicture'
 
 
 function App() {
   return (
     <>
-   {fruits.length > 0 &&  <List items={fruits} category="Fruits" />}
-   {vegetables.length > 0 &&  <List items={vegetables} category="Vegetables" />}
+    
+    
+    <ProfilePicture />
+  
 
 
     </>
---
+++ Review1/src/Button.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Button() {
+
+    
+
+    const handleClick = e.target.display = "none";
+    
+
+  return (
+    <button onClick={(e) => handleClick(e) }> Click Me </button>
+  )
+}
+
+export default Button
