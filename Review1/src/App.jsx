d80a6ddd52f85d647dc0691ba3ca1e1c40408712
sara <saradernouni@gmail.com>
2025-01-07 07:05:33 -0500

    3

--- Review1/src/App.jsx
+++ Review1/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import Student from './Student'
+import UserGreeting from './UserGreeting'
+
 
 
 
@@ -7,10 +8,7 @@ import Student from './Student'
 function App() {
   return (
     <>
-    <Student name="Spongebob" age={30} isStudent={false} />
-    <Student name="Zion" age={13} isStudent={true} />
-    <Student />
-
+   <UserGreeting isLoggedIn={true} username={12} />
 
     </>
   )
